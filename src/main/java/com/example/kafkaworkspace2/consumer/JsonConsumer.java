72233027b7603df47b99535a9df225dc85ffe594
KingBlackCow <sgs1159@naver.com>
2024-01-31 19:27:03 +0900

    feat: consumer 멱등성 보장하기

--- src/main/java/com/example/kafkaworkspace2/consumer/JsonConsumer.java
+++ src/main/java/com/example/kafkaworkspace2/consumer/JsonConsumer.java
@@ -7,18 +7,34 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import static com.example.kafkaworkspace2.model.Topic.JS_JSON_TOPIC;
 
 @Component
 @Slf4j
 public class JsonConsumer {
+
+    private final Map<String, Integer> idHistoryMap = new ConcurrentHashMap<>();
+
     @KafkaListener(
-            topics = { JS_JSON_TOPIC },
+            topics = {JS_JSON_TOPIC},
             groupId = "js-json-consumer-group"
     )
     public void accept(ConsumerRecord<String, JsMessage> message, Acknowledgment acknowledgment) {
         log.info("[json consumer] - " + message.value());
         // 수동 커밋 설정
-         acknowledgment.acknowledge();
+        this.printPayloadIfFirstMessage(message.value());
+        acknowledgment.acknowledge();
+    }
+
+    private void printPayloadIfFirstMessage(JsMessage jsMessage) {
+        if (idHistoryMap.putIfAbsent(String.valueOf(jsMessage.getId()), 1) == null) {
+            log.info("[json first consumer] - " + jsMessage);
+            idHistoryMap.put(String.valueOf(jsMessage.getId()), 1);
+        } else {
+            System.out.println("[json consumer] - Duplicate! id: " + jsMessage.getId());
+        }
     }
 }
