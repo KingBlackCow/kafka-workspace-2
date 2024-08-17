e7c76f819ffa41cd9f2c92a525c2f69fd5fdf84a
KingBlackCow <sgs1159@naver.com>
2024-08-17 15:01:32 +0900

    feat: string consumer

--- src/main/java/com/example/kafkaworkspace2/consumer/StringConsumer.java
+++ src/main/java/com/example/kafkaworkspace2/consumer/StringConsumer.java
@@ -1,22 +1,22 @@
-//package com.example.kafkaworkspace2.consumer;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.kafka.clients.consumer.ConsumerRecord;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Component;
-//
-//import static com.example.kafkaworkspace2.model.Topic.JS_STRING_TOPIC;
-//
-//@Component
-//@Slf4j
-//public class StringConsumer {
-//    @KafkaListener(
-//            topics = {JS_STRING_TOPIC},
-//            groupId = "string-consumer-group",
-//            containerFactory = "secondKafkaListenerContainerFactory"
-//    )
-//    public void accept(ConsumerRecord<String, String> message) {
-//        log.info("[String Consumer] Message arrived! - " + message.value());
-//        log.info("[String Consumer] Offset - " + message.offset() + " / Partition - " + message.partition());
-//    }
-//}
+package com.example.kafkaworkspace2.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import static com.example.kafkaworkspace2.model.Topic.JS_STRING_TOPIC;
+
+@Component
+@Slf4j
+public class StringConsumer {
+    @KafkaListener(
+            topics = {JS_STRING_TOPIC},
+            groupId = "string-consumer-group",
+            containerFactory = "secondKafkaListenerContainerFactory"
+    )
+    public void accept(ConsumerRecord<String, String> message) {
+        log.info("[String Consumer] Message arrived! - " + message.value());
+        log.info("[String Consumer] Offset - " + message.offset() + " / Partition - " + message.partition());
+    }
+}
